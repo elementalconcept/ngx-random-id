454ab755f9a687b6cff97102254739babc31e9c0
simonepachera <pachera.simone@gmail.com>
2023-03-04 13:53:05 +0000

    Initital lib

---
+++ projects/lib/src/lib/random-id.service.ts
@@ -0,0 +1,49 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class RandomIdService {
+  private readonly defaultSeparator = '-';
+  private readonly defaultParts = 4;
+
+  getRandomIdTimestamp = (separator = this.defaultSeparator,
+                          prefix: string | null = null,
+                          suffix: string | null = null): string => {
+
+    let randomId = new Date().getTime().toString();
+
+    if (prefix !== null && prefix.length > 0) {
+      randomId = prefix + separator + randomId;
+    }
+
+    if (suffix !== null && suffix.length > 0) {
+      randomId = randomId + separator + suffix;
+    }
+
+    return randomId;
+  };
+
+  getRandomId = (parts = this.defaultParts,
+                 separator = this.defaultSeparator,
+                 prefix: string | null = null,
+                 suffix: string | null = null) => {
+
+    const array = [];
+
+    if (prefix !== null && prefix.length > 0) {
+      array.push(prefix);
+    }
+
+    for (let i = 0; i < parts; i++) {
+      // tslint:disable-next-line:no-bitwise
+      array.push((((1 + Math.random()) * 0x10000) | 0).toString(16).substring(1));
+    }
+
+    if (suffix !== null && suffix.length > 0) {
+      array.push(suffix);
+    }
+
+    return array.join(separator);
+  };
+}
